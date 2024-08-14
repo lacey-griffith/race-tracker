d035386eb52be2e79faf6289f21f108166633504
Lacey <lacey.griffith04@gmail.com>
2024-08-14 12:38:15 -0500

    logo styling and sizing

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 const NavBar = () => {
     return (
         <nav>
-            <img class='nav-logo' src=''></img>
+            <img class='nav-logo' src='/121.png' alt='red, elongated hexagon with white inside it in that reads 12:1 in larger font and running in smaller font underneath'/>
             <ul>
                 <li><Link to ='/'>Home</Link></li>
                 <li><Link to ='/about'>About</Link></li>
