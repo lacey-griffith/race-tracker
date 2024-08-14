7b76e326f4f109b16936efd060d5db138387566e
Lacey <lacey.griffith04@gmail.com>
2024-08-14 12:30:19 -0500

    add logo

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 const NavBar = () => {
     return (
         <nav>
+            <img class='nav-logo' src=''></img>
             <ul>
                 <li><Link to ='/'>Home</Link></li>
                 <li><Link to ='/about'>About</Link></li>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,7 @@ const HomePage = () => {
         <>
         <h1>Welcome to 12:1 Running</h1>
         <p>
-            Welcome to my blog! Proin congue
-            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+            Proin congue ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
             Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
             non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
             eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
